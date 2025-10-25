f5041fde38a18822486bc5d1ff7e43749442d574
Mst Surnaly Akter <surnalyms@gmail.com>
2025-10-25 19:49:18 +0600

    Add DiamondPattern class and modify PalindromicPattern output formatting

--- first/PalindromicPattern.java
+++ first/PalindromicPattern.java
@@ -8,12 +8,11 @@ public class PalindromicPattern {
             }
             for (int j = i; j >= 1; j--) {
                 System.out.print(j);
-                  System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
                if (j > 1) {
                  System.out.print(j);
-                  System.out.print(" ");
+               
                }
             }
             System.out.println();
